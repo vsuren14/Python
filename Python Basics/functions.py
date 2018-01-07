4f9398ff04676f8e6b6a0076a37ca5ed6452ea8d
Suren <vsuren14@gmail.com>
2018-01-07 19:33:47 +0530

    final commit

---
+++ Python Basics/functions.py
@@ -0,0 +1,17 @@
+def hows_the_parrot():
+    print("He's pining for the fjords!")
+
+hows_the_parrot()
+
+def lumberjack(name, pronoun):
+        print("{}'s a lumberjack and {} OK!".format(name, pronoun))
+
+lumberjack("Kenneth", "he's")
+lumberjack("Lacey", "she's")
+lumberjack("Sam", "they're")
+
+def average(num1, num2):
+    return (num1+num2) / 2
+
+avg = average(2, 8)
+print(avg)
